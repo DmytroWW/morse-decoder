9f2fc8cd9b2a8737707fde129c8e13cfe7382b01
DmytroWW <veksbrightside@gmail.com>
2024-09-27 21:19:02 +0300

    feat: add solving

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+   const parts = [];
+	let result = '';
+	for (let i = 0; i < expr.length; i += 10) {
+		parts.push(expr.slice(i, i + 10)); 
+	}
+	parts.forEach(part => {
+		let morseCode = '';
+		if (part === '**********'){
+			result += ' ';
+		}
+		else {
+			
+			for (let i = 0; i < 10; i += 2) {
+				const pair = part.slice(i, i+2);
+				if (pair === '10') { morseCode += '.';
+				}
+				if (pair === '11') { morseCode += '-';
+				}	
+			}
+		}
+		if (morseCode && MORSE_TABLE[morseCode]) {
+                result += MORSE_TABLE[morseCode];
+            } else if (morseCode) {
+                result += '?';
+            }
+	});
+
+	return result;
+	
 }
 
 module.exports = {
